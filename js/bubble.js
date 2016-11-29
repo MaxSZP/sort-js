ff0f30118f4c9e0c0954f9a9a15e62e4f9614e1d
werder33 <andreywow90@hmail.com>
2016-11-29 22:48:11 +0200

    gnome sort and fix bubblesort

--- js/bubble.js
+++ js/bubble.js
@@ -1,4 +1,3 @@
-
 var vm = new Vue({
     el: '#app',
     data: {
@@ -16,7 +15,7 @@ var vm = new Vue({
     },
     filters: {
         toSecond: function (value) {
-            return value/1000 + ' c.'
+            return value / 1000 + ' c.'
         }
     },
     methods: {
@@ -34,78 +33,42 @@ var vm = new Vue({
             return Math.floor(Math.random() * this.items.length)
         },
         add: function (i, obj) {
-            console.log(obj);
             this.items.splice(i, 0, obj)
         },
         remove: function (i) {
             this.items.splice(i, 1)
         },
         shuffle: function () {
+            vm.items.forEach(function (item, i, arr) {
+                arr[i].active = false;
+            });
             this.items = _.shuffle(this.items)
-        },
-        sort: function () {
-            console.log(this.i);
-            /* var j = 0;
-             if (this.i < 5) {
-             this.i++;
-             console.log(this.items.length);
-             /!* setTimeout(function () {
-             sort();
-             }, 2000);*!/
-             }
-             else {
-             console.log('end');
-             }*/
-
-
-            /*
-             * for(j=1;j<ar.length-1;j++){
-             coun++;
-             for(i=1;i<ar.length;i++){
-             if(ar[i]<ar[i-1]){
-             temp = ar[i];
-             ar[i] = ar[i-1];
-             ar[i-1] = temp;
-             coun++;
-             }
-             else coun++;
-             }
-             }*/
         },
         animationSort: function () {
             var j = 1;
             var timer = setTimeout(function g() {
                 var i = 1;
                 var timerId = setTimeout(function go() {
-                    console.log(vm.items.length);
-                    vm.items[i-1].active = false;
-                    if (i >= 2) {
-                        vm.items[i - 2].active = false;
-                    }
-
+                    vm.items.forEach(function (item, i, arr) {
+                        arr[i].active = false;
+                    });
+                    console.log(i);
                     vm.items[i].active = true;
                     vm.items[i - 1].active = true;
                     if (vm.items[i].id < vm.items[i - 1].id) {
-                        //console.log(i);
-                            var tmp = vm.items[i - 1];
-                            console.log(tmp);
-                            vm.remove(i - 1);
-                            vm.add(i, tmp);
-                        /* temp = vm.items[i];
-                         vm.items[i] = vm.items[i - 1];
-                         vm.items[i - 1] = temp;*/
+
+                        var tmp = vm.items[i - 1];
+                        vm.remove(i - 1);
+                        vm.add(i, tmp);
                     }
 
-                    if (i < vm.items.length) setTimeout(go, vm.animationSpeed);
+                    if (i < vm.items.length /*- (j-1)*/) setTimeout(go, vm.animationSpeed);
                     i++;
                 }, 100);
                 if (j < vm.items.length - 1) setTimeout(g, vm.animationSpeed * vm.items.length);
                 j++;
             }, 100);
 
-            var r = setTimeout(function tr() {
-                console.log(vm.items);
-            }, 42000);
 
         }
     }
---
+++ js/gnome.js
@@ -0,0 +1,110 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        items: [],
+        step: 0,
+        animationSpeed: 2000,
+        maxAnimationSpeed: 5000,
+        tmp: {},
+        i: 0,
+        arr: [4, 5, 2, 10, 5]
+    },
+    created: function () {
+        this.generateArray();
+        //  console.log(this.items);
+    },
+    filters: {
+        toSecond: function (value) {
+            return value / 1000 + ' c.'
+        }
+    },
+    methods: {
+        generateArray: function () {
+            for (var i = 0; i < 9; i++) {
+                this.items.push({
+                    id: i,
+                    height: 50 + this.step + 'px',
+                    active: false
+                });
+                this.step = this.step + 15;
+            }
+        },
+        randomIndex: function () {
+            return Math.floor(Math.random() * this.items.length)
+        },
+        add: function (i, obj) {
+            this.items.splice(i, 0, obj)
+        },
+        remove: function (i) {
+            this.items.splice(i, 1)
+        },
+        shuffle: function () {
+            vm.items.forEach(function(item, i, arr) {
+                arr[i].active = false;
+            });
+            this.items = _.shuffle(this.items)
+        },
+        animationSort: function () {
+           // var ArrayBySort = [2,7,22,6,76,9,6546,27];
+
+            var n = vm.items.length, i = 1, j = 2;
+            var timerId = setTimeout(function go() {
+
+                vm.items.forEach(function(item, i, arr) {
+                    arr[i].active = false;
+                });
+                vm.items[i].active = true;
+                vm.items[i - 1].active = true;
+                if (vm.items[i - 1].id < vm.items[i].id) {
+                    /*i = j;
+                    j++;*/
+                    i++;
+                }
+                else {
+                    var tmp = vm.items[i - 1];
+                    // console.log(tmp);
+                    vm.remove(i - 1);
+                    vm.add(i, tmp);
+                    i--;
+                    if (i == 0) {
+                        /*i = j;
+                        j++;*/
+                        i++;
+                    }
+                }
+
+                if(i == n ) {
+                    vm.items.forEach(function(item, i, arr) {
+                        arr[i].active = true;
+                    });
+                }
+                if (i < n) setTimeout(go, vm.animationSpeed);
+            }, 100);
+
+
+        },
+        gnomeSort: function (ArrayBySort) {
+            var count = 0;
+            var n = ArrayBySort.length, i = 1, j = 2;
+            while (i < n) {
+                if (ArrayBySort[i - 1] < ArrayBySort[i]) {
+                    i = j;
+                    j++;
+                }
+                else {
+                    var t = ArrayBySort[i - 1];
+                    ArrayBySort[i - 1] = ArrayBySort[i];
+                    ArrayBySort[i] = t;
+                    i--;
+                    if (i == 0) {
+                        i = j;
+                        j++;
+                    }
+                }
+                count++;
+            }
+            this.count = count;
+            return ArrayBySort;
+        }
+    }
+});
